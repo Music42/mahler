45086db4a5074c8c35024f4e53bb072948963262
Cristian Dima <cristian@cristiandima.com>
2015-10-05 18:38:33 +0300

    Moved midi code into main script, added midi list Refactored some code: moved midi code into main script, created new helper functions, reorganized code a little bit. Added a midi list display which shows the connected midi devices or an error message if no midi support is detected.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*global SheetMusic, Piano */
+/*global Note, SheetMusic, Piano */
 
 var CANVAS = document.getElementById('game-board');
 var CTX = CANVAS.getContext('2d');
@@ -6,11 +6,67 @@ var SETTINGS = getOptions();
 var SHEET_MUSIC = new SheetMusic(SETTINGS);
 var PIANO_WIDTH = 300;
 var PIANO = new Piano(CTX, calcPianoX(), calcPianoY(), PIANO_WIDTH, 150);
-
-PIANO.click(function() {
-    this.draw();
-    checkUserInput(this.pressedKeys.toArray(), false);
+var MIDI_NOTES = new HashSet(function(note) {
+    return note.code;
 });
+displaySavedSettings();
+setUpMidi();
+setUpPiano(PIANO);
+setUpTheButton();
+
+function displaySavedSettings() {
+    var settings = localStorage.getItem('mahler-settings');
+    if (settings !== null) {
+        settings = JSON.parse(settings);
+        $('#selMode').val(settings.mode);
+        $('#selKey').val(settings.key);
+        $('#selClef').val(settings.clef);
+    }
+}
+
+function setUpMidi() {
+    if (navigator.requestMIDIAccess) {
+        navigator.requestMIDIAccess({
+            sysex: false
+        }).then(onMIDISuccess, onMIDIFailure);
+    } else {
+        var msg = 'No MIDI support. This is available in Chrome 43+.';
+        $('.midi-devices').append(makeLiElement(msg));
+    }
+}
+
+function setUpPiano(piano) {
+    piano.click(function() {
+        this.draw();
+        checkUserInput(this.pressedKeys.toArray(), false);
+    });
+}
+
+function setUpTheButton() {
+    $('#btn-start').click(function() {
+        var mainBtn = this;
+        var startClick = true;
+        if ($(mainBtn).hasClass('clicked')) {
+            $(mainBtn).removeClass('clicked');
+            startClick = false;
+        } else {
+            $(mainBtn).addClass('clicked');
+        }
+        if (startClick) {
+            updateLocalSettings();
+            $('#game-menu').fadeOut(300, function() {
+                updateGame();
+                $('#game-board').show();
+                $(mainBtn).text('OPTIONS');
+            });
+        } else {
+            $('#game-board').fadeOut(300, function() {
+                $('#game-menu').show();
+                $(mainBtn).text('START');
+            });
+        }
+    });
+}
 
 function checkUserInput(pressedKeys, strict) {
     if (compareKeys(SHEET_MUSIC.nextNotes(), pressedKeys, strict)) {
@@ -79,36 +135,58 @@ function updateLocalSettings() {
     localStorage.setItem('mahler-settings', JSON.stringify(settings));
 }
 
-$('#btn-start').click(function() {
-    var mainBtn = this;
-    var startClick = true;
-    if ($(mainBtn).hasClass('clicked')) {
-        $(mainBtn).removeClass('clicked');
-        startClick = false;
-    } else {
-        $(mainBtn).addClass('clicked');
-    }
-    if (startClick) {
-        updateLocalSettings();
-        $('#game-menu').fadeOut(300, function() {
-            updateGame();
-            $('#game-board').show();
-            $(mainBtn).text('OPTIONS');
-        });
-    } else {
-        $('#game-board').fadeOut(300, function() {
-            $('#game-menu').show();
-            $(mainBtn).text('START');
-        });
-    }
-});
+function onMIDISuccess(midiAccess) {
+    var midi = midiAccess;
+    var inputs = midi.inputs.values();
+    setUpMIDIDevices(inputs);
+    midi.onstatechange = onStateChange;
+}
 
-$(document).ready(function() {
-    var settings = localStorage.getItem('mahler-settings');
-    if (settings !== null) {
-        settings = JSON.parse(settings);
-        $('#selMode').val(settings.mode);
-        $('#selKey').val(settings.key);
-        $('#selClef').val(settings.clef);
+function setUpMIDIDevices(inputs) {
+    $('.midi-devices').empty();
+    var input;
+    for (input = inputs.next(); input && !input.done; input = inputs.next()) {
+        input.value.onmidimessage = onMIDIMessage;
+        var lowerName = input.value.name.toLowerCase();
+        if (lowerName.indexOf('midi through port') === -1) {
+            $('.midi-devices').append(makeLiElement(input.value.name));
+        }
     }
-});
+    if ($('.midi-devices').children().length === 0) {
+        $('.midi-devices').append(makeLiElement('No MIDI device connected'));
+    }
+}
+
+function onMIDIFailure() {
+    var msg =
+        'No access to MIDI devices' +
+        'or your browser doesn\'t support WebMIDI API';
+    $('.midi-devices').append(makeLiElement(msg));
+}
+
+function onStateChange() {
+    var inputs = this.inputs.values();
+    setUpMIDIDevices(inputs);
+}
+
+function onMIDIMessage(event) {
+    var data = event.data;
+    var cmd = data[0] === 128 ? 'keyup' : 'keydown';
+    var code = data[1];
+    if (cmd === 'keydown') {
+        MIDI_NOTES.add(new Note(code, SETTINGS.key));
+        checkUserInput(MIDI_NOTES.toArray(), true);
+    }
+    if (cmd === 'keyup') {
+        MIDI_NOTES.remove({
+            code: code
+        });
+    }
+}
+
+function makeLiElement(text) {
+    return $('<li/>', {
+        'text': text,
+        'class': 'list-group-item'
+    });
+}
